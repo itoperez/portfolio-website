8d9fe50ea5209c61c6e7185a4c8910639dc26265
Ito Perez <96797555+itoperez@users.noreply.github.com>
2025-08-29 14:11:19 -0400

    Articles and Shader Thumbnails
    Article resize to fit on mobile and removing onHover for touch devices

--- js/index.js
+++ js/index.js
@@ -11,6 +11,32 @@ navLinks.forEach((link) => {
     });
 });
 
+function watchForHover() {
+    // lastTouchTime is used for ignoring emulated mousemove events
+    let lastTouchTime = 0;
+
+    function enableHover() {
+        if (new Date() - lastTouchTime < 500) return;
+        document.body.classList.add("hasHover");
+    }
+
+    function disableHover() {
+        document.body.classList.remove("hasHover");
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener("touchstart", updateLastTouchTime, true);
+    document.addEventListener("touchstart", disableHover, true);
+    document.addEventListener("mousemove", enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
+
 document.querySelectorAll(".portfolio__videos video").forEach((vid) => {
     vid.onclick = () => {
         document.querySelector(".portfolio__popup_video").style.display = "block";
